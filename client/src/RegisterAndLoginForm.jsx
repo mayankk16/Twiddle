a6a08223ec1c8f32a6a6a0a14ed51ae1a14f17c3
manthan2004 <bhatmanthan04@gmail.com>
2024-07-02 11:15:37 +0530

    improve design of register/login page

--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -1,50 +1,146 @@
-import {useContext, useState} from "react";
+// import {useContext, useState} from "react";
+// import axios from "axios";
+// import {UserContext} from "./User_Context.jsx";
+
+// export default function RegisterAndLoginForm() {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [isLoginOrRegister, setIsLoginOrRegister] = useState('login');
+//   const {setUsername:setLoggedInUsername, setId} = useContext(UserContext);
+//   async function handleSubmit(ev) {
+//     ev.preventDefault();
+//     const url = isLoginOrRegister === 'register' ? '/register' : '/login';
+//     const {data} = await axios.post(url, {username,password});
+//     setLoggedInUsername(username);
+//     setId(data.id);
+//   }
+//   return (
+//     <div className="bg-blue-50 h-screen flex items-center">
+//       <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
+//         <input value={username}
+//                onChange={ev => setUsername(ev.target.value)}
+//                type="text" placeholder="username"
+//                className="block w-full rounded-sm p-2 mb-2 border" />
+//         <input value={password}
+//                onChange={ev => setPassword(ev.target.value)}
+//                type="password"
+//                placeholder="password"
+//                className="block w-full rounded-sm p-2 mb-2 border" />
+//         <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+//           {isLoginOrRegister === 'register' ? 'Register' : 'Login'}
+//         </button>
+//         <div className="text-center mt-2">
+//           {isLoginOrRegister === 'register' && (
+//             <div>
+//               Already a member?
+//               <button className="ml-1" onClick={() => setIsLoginOrRegister('login')}>
+//                 Login here
+//               </button>
+//             </div>
+//           )}
+//           {isLoginOrRegister === 'login' && (
+//             <div>
+//               Dont have an account?
+//               <button className="ml-1" onClick={() => setIsLoginOrRegister('register')}>
+//                 Register
+//               </button>
+//             </div>
+//           )}
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+import { useContext, useState } from "react";
 import axios from "axios";
-import {UserContext} from "./User_Context.jsx";
+import { UserContext } from "./User_Context.jsx";
+import "./styles.css"; // Ensure your CSS file is imported
+import Logo from "./logo.jsx";
 
 export default function RegisterAndLoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isLoginOrRegister, setIsLoginOrRegister] = useState('login');
-  const {setUsername:setLoggedInUsername, setId} = useContext(UserContext);
+  const [error, setError] = useState(null); // State for error message
+
+  const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
+
   async function handleSubmit(ev) {
     ev.preventDefault();
-    const url = isLoginOrRegister === 'register' ? '/register' : '/login';
-    const {data} = await axios.post(url, {username,password});
-    setLoggedInUsername(username);
-    setId(data.id);
+    const url = isLoginOrRegister === 'register' ? 'register' : 'login';
+    try {
+      const { data } = await axios.post(url, { username, password });
+      setLoggedInUsername(username);
+      setId(data.id);
+    } catch (error) {
+      console.error("Error during login/register:", error.response);
+      setError(error.response.data); // Set error message received from server
+    }
   }
+
   return (
-    <div className="bg-blue-50 h-screen flex items-center">
-      <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
-        <input value={username}
-               onChange={ev => setUsername(ev.target.value)}
-               type="text" placeholder="username"
-               className="block w-full rounded-sm p-2 mb-2 border" />
-        <input value={password}
-               onChange={ev => setPassword(ev.target.value)}
-               type="password"
-               placeholder="password"
-               className="block w-full rounded-sm p-2 mb-2 border" />
-        <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
-          {isLoginOrRegister === 'register' ? 'Register' : 'Login'}
-        </button>
-        <div className="text-center mt-2">
-          {isLoginOrRegister === 'register' && (
-            <div>
-              Already a member?
-              <button className="ml-1" onClick={() => setIsLoginOrRegister('login')}>
+    <div className="background-image full-screen">
+      <Logo />
+      <form className="form-container" onSubmit={handleSubmit}>
+        {error && ( // Render error message if error state is set
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+            {error}
+          </div>
+        )}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Username
+          </label>
+          <input
+            value={username}
+            onChange={(ev) => setUsername(ev.target.value)}
+            type="text"
+            placeholder="Enter your username"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Password
+          </label>
+          <input
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+            type="password"
+            placeholder="********"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex items-center justify-between mb-4">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            {isLoginOrRegister === 'register' ? 'Register' : 'Login'}
+          </button>
+        </div>
+        <div className="text-center mt-2 text-sm text-gray-600">
+          {isLoginOrRegister === 'register' ? (
+            <p>
+              Already a member?{' '}
+              <button
+                className="text-blue-500 focus:outline-none"
+                onClick={() => setIsLoginOrRegister('login')}
+              >
                 Login here
               </button>
-            </div>
-          )}
-          {isLoginOrRegister === 'login' && (
-            <div>
-              Dont have an account?
-              <button className="ml-1" onClick={() => setIsLoginOrRegister('register')}>
+            </p>
+          ) : (
+            <p>
+              Don't have an account?{' '}
+              <button
+                className="text-blue-500 focus:outline-none"
+                onClick={() => setIsLoginOrRegister('register')}
+              >
                 Register
               </button>
-            </div>
+            </p>
           )}
         </div>
       </form>
--- client/src/chat.jsx
+++ client/src/chat.jsx
@@ -1,54 +1,52 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import Avatar from "./avatar.jsx";
-import Logo from "./logo.jsx";
-import Contact from "./Contact";
-import { UserContext } from "./User_Context.jsx";
-import { uniqBy } from "lodash";
+import Logo from "./logo";
+import {UserContext} from "./User_Context.jsx";
+import {uniqBy} from "lodash";
 import axios from "axios";
+import Contact from "./Contact";
 
 export default function Chat() {
-  const [ws, setWs] = useState(null);
-  const [onlinePeople, setOnlinePeople] = useState({});
+  const [ws,setWs] = useState(null);
+  const [onlinePeople,setOnlinePeople] = useState({});
   const [offlinePeople,setOfflinePeople] = useState({});
-  const [selectedUserId, setSelectedUserId] = useState(null);
-  const [newMessageText, setNewMessageText] = useState('');
-  const [messages, setMessages] = useState([]);
-  const { username, id,setId,setUsername } = useContext(UserContext);
+  const [selectedUserId,setSelectedUserId] = useState(null);
+  const [newMessageText,setNewMessageText] = useState('');
+  const [messages,setMessages] = useState([]);
+  const {username,id,setId,setUsername} = useContext(UserContext);
   const divUnderMessages = useRef();
-
   useEffect(() => {
+    connectToWs();
+  }, [selectedUserId]);
+  function connectToWs() {
+    const ws = new WebSocket('ws://localhost:4040');
+    setWs(ws);
+    ws.addEventListener('message', handleMessage);
+    ws.addEventListener('close', () => {
+      setTimeout(() => {
+        console.log('Disconnected. Trying to reconnect.');
         connectToWs();
-      }, [selectedUserId]);
-      function connectToWs() {
-        const ws = new WebSocket('ws://localhost:4040');
-        setWs(ws);
-        ws.addEventListener('message', handleMessage);
-        ws.addEventListener('close', () => {
-          setTimeout(() => {
-            console.log('Disconnected. Trying to reconnect.');
-            connectToWs();
-          }, 1000);
-        });
-      }
-
+      }, 1000);
+    });
+  }
   function showOnlinePeople(peopleArray) {
     const people = {};
-    peopleArray.forEach(({ userId, username }) => {
+    peopleArray.forEach(({userId,username}) => {
       people[userId] = username;
     });
     setOnlinePeople(people);
   }
-
   function handleMessage(ev) {
     const messageData = JSON.parse(ev.data);
-    console.log({ ev, messageData });
+    console.log({ev,messageData});
     if ('online' in messageData) {
       showOnlinePeople(messageData.online);
     } else if ('text' in messageData) {
-      setMessages(prev => uniqBy([...prev, { ...messageData }], '_id'));
+      if (messageData.sender === selectedUserId) {
+        setMessages(prev => ([...prev, {...messageData}]));
+      }
     }
   }
-
   function logout() {
     axios.post('/logout').then(() => {
       setWs(null);
@@ -56,21 +54,6 @@ export default function Chat() {
       setUsername(null);
     });
   }
-
-  // function sendMessage(ev) {
-  //   ev.preventDefault();
-  //   ws.send(JSON.stringify({
-  //     recipient: selectedUserId,
-  //     text: newMessageText,
-  //   }));
-  //   setNewMessageText('');
-  //   setMessages(prev => ([...prev,{
-  //     text: newMessageText,
-  //     sender: id,
-  //     recipient: selectedUserId,
-  //     _id: Date.now(),
-  //   }]));
-  // }
   function sendMessage(ev, file = null) {
     if (ev) ev.preventDefault();
     ws.send(JSON.stringify({
@@ -131,22 +114,23 @@ export default function Chat() {
     }
   }, [selectedUserId]);
 
-  const onlinePeopleExclOurUser = { ...onlinePeople };
+  const onlinePeopleExclOurUser = {...onlinePeople};
   delete onlinePeopleExclOurUser[id];
+
   const messagesWithoutDupes = uniqBy(messages, '_id');
 
   return (
-    <div  className="flex h-screen">
-      <div  className="bg-white w-1/3 flex flex-col">
-      <div className="flex-grow">
-        <Logo />
-        {Object.keys(onlinePeopleExclOurUser).map(userId => (
+    <div className="flex h-screen">
+      <div className="bg-white w-1/3 flex flex-col">
+        <div className="flex-grow">
+          {/* <Logo /> */}
+          {Object.keys(onlinePeopleExclOurUser).map(userId => (
             <Contact
               key={userId}
               id={userId}
               online={true}
               username={onlinePeopleExclOurUser[userId]}
-              onClick={() => setSelectedUserId(userId)}
+              onClick={() => {setSelectedUserId(userId);console.log({userId})}}
               selected={userId === selectedUserId} />
           ))}
           {Object.keys(offlinePeople).map(userId => (
@@ -158,20 +142,19 @@ export default function Chat() {
               onClick={() => setSelectedUserId(userId)}
               selected={userId === selectedUserId} />
           ))}
-      </div>
-      <div className="p-2 text-center flex items-center justify-center">
-        <span className="mr-2 text-sm text-gray-600 flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
-          </svg>
+        </div>
+        <div className="p-2 text-center flex items-center justify-center">
+          <span className="mr-2 text-sm text-gray-600 flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+              <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+            </svg>
             {username}
           </span>
-        <button
+          <button
             onClick={logout}
             className="text-sm bg-blue-100 py-1 px-2 text-gray-500 border rounded-sm">logout</button>
+        </div>
       </div>
-    </div>
-    
       <div className="flex flex-col bg-blue-50 w-2/3 p-2">
         <div className="flex-grow">
           {!selectedUserId && (
@@ -183,8 +166,8 @@ export default function Chat() {
             <div className="relative h-full">
               <div className="overflow-y-scroll absolute top-0 left-0 right-0 bottom-2">
                 {messagesWithoutDupes.map(message => (
-                  <div key={message._id} className={(message.sender === id ? 'text-right' : 'text-left')}>
-                    <div className={"text-left inline-block p-2 my-2 rounded-md text-sm " + (message.sender === id ? 'bg-blue-500 text-white' : 'bg-white text-gray-500')}>
+                  <div key={message._id} className={(message.sender === id ? 'text-right': 'text-left')}>
+                    <div className={"text-left inline-block p-2 my-2 rounded-md text-sm " +(message.sender === id ? 'bg-blue-500 text-white':'bg-white text-gray-500')}>
                       {message.text}
                       {message.file && (
                         <div className="">
@@ -206,14 +189,12 @@ export default function Chat() {
         </div>
         {!!selectedUserId && (
           <form className="flex gap-2" onSubmit={sendMessage}>
-            <input
-              type="text"
-              value={newMessageText}
-              onChange={ev => setNewMessageText(ev.target.value)}
-              placeholder="Type your message here"
-              className="bg-white flex-grow border rounded-sm p-2"
-            />
-              <label className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
+            <input type="text"
+                   value={newMessageText}
+                   onChange={ev => setNewMessageText(ev.target.value)}
+                   placeholder="Type your message here"
+                   className="bg-white flex-grow border rounded-sm p-2"/>
+            <label className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
               <input type="file" className="hidden" onChange={sendFile} />
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                 <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
@@ -230,4 +211,5 @@ export default function Chat() {
     </div>
   );
 }
+
 
